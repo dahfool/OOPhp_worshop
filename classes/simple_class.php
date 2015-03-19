4fb6c16b9c60bd468f46d8461f61c40fa7f756f6
Gerald Isabelle <gerald.isabelle@cohaesus.co.uk>
2015-03-19 00:59:47 +0000

    lesson 1 classes

---
+++ classes/simple_class.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Creating a simple class
+ *
+ * Class class_name
+ */
+class class_name {
+
+    /**
+     * Declaring a property
+     */
+    public $property = 'i am property';
+
+    /**
+     * Declaring a method
+     */
+    public function new_method()
+    {
+        echo 'i am a method';
+    }
+
+}
+
+
+//$object = new class_name;
+
+//echo($object->property);
+
+//echo($object->new_method());
